aac1ab2ac66893da10190d3650b1658b2bb05e78
symons <somborisymonss@gmail.com>
2019-11-05 11:33:25 +0300

    more changes

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('course/<int:pk>/', views.course_detail, name='course_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Course
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
-    return render(request, 'home.html', {})
+    courses = Course.objects.all()
+    return render(request, 'index.html', {'courses':courses})
 
+def course_detail(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    return render(request, 'detail.html', {'course':course})
